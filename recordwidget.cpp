7e39cce543a0c7fcceb467c5c01873b40d59d838
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2017-01-29 21:44:50 +0100

    Added basic metronome

--- recordwidget.cpp
+++ recordwidget.cpp
@@ -12,6 +12,9 @@ RecordWidget::RecordWidget(RtMidiIn& midi_in, RtMidiOut& midi_out) :
     _midi_out(midi_out)
 {
     ui->setupUi(this);
+
+    connect(&_bpm_timer,&QTimer::timeout, this, &RecordWidget::onBpmTimeout);
+    on_bpm_valueChanged();
 }
 
 RecordWidget::~RecordWidget()
@@ -34,11 +37,52 @@ void RecordWidget::on_record_clicked(bool enabled)
 {
     if(enabled) {
         _track.clear();
-        _midi_in.setCallback(RecordWidget::onMidiEvent, this);
+        _midi_in.setCallback(&RecordWidget::onMidiEvent, this);
         _time.start();
     } else {
         _midi_in.cancelCallback();
     }
+}
+
+void RecordWidget::on_metronome_clicked(bool enabled)
+{
+    if(enabled) {
+        _current_beat = 0;
+        onBpmTimeout();
+        _bpm_timer.start();
+    } else {
+        _bpm_timer.stop();
+    }
+}
+
+void RecordWidget::onBpmTimeout()
+{
+    enum MidiValue {
+        NOTE_ON = 0b10010000,
+        NOTE_OFF = 0b10000000,
+
+        BEAT_NOTE = 0x25,
+        CHANNEL = 9,
+        VELOCITY = 60,
+    };
+
+    std::vector<unsigned char> note = {
+        MidiValue::NOTE_ON + MidiValue::CHANNEL,
+        MidiValue::BEAT_NOTE,
+        MidiValue::VELOCITY
+    };
+    if(_current_beat == 0) {
+        note[2] += 40;
+    }
+    _midi_out.sendMessage(&note);
+    _current_beat = (_current_beat + 1) % ui->beats->value();
+    note[0] = MidiValue::NOTE_OFF + MidiValue::CHANNEL;
+    _midi_out.sendMessage(&note);
+}
+
+void RecordWidget::on_bpm_valueChanged(int)
+{
+    _bpm_timer.setInterval(60000 / ui->bpm->value());
 }
 
 void RecordWidget::timerEvent(QTimerEvent*)
--- recordwidget.h
+++ recordwidget.h
@@ -4,6 +4,7 @@
 #include <QDockWidget>
 #include <QVector>
 #include <QTime>
+#include <QTimer>
 
 #include "RtMidi.h"
 
@@ -25,9 +26,12 @@ public:
     explicit RecordWidget(RtMidiIn& midi_in, RtMidiOut& midi_out);
     ~RecordWidget();
 
-public slots:
+private slots:
     void on_play_clicked(bool);
     void on_record_clicked(bool);
+    void on_metronome_clicked(bool);
+    void onBpmTimeout();
+    void on_bpm_valueChanged(int = 0);
 
 protected:
       void timerEvent(QTimerEvent *event);
@@ -42,6 +46,8 @@ private:
     Track _track;
     Track::Iterator _cursor;
     int _timer_id;
+    QTimer _bpm_timer;
+    quint8 _current_beat;
 };
 
 #endif // RECORDWIDGET_H
